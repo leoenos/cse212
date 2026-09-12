b18789e455d8f81a2e4c701fd63c3c65dce798bf
leoenos <senos@byupathway.edu>
2026-09-12 15:43:16 -0600

    multiples of function

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,13 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // The function should create and return an array of multiples of a number
+        double[] multiples = new double[length]; // Instaniates an array of doubles of the needed length
+        for (int i = 0; i < length; i++) // Loops through every index in the array and calculates the multiple
+        {
+            multiples[i] = number * (i + 1);
+        }
+        return multiples;
     }
 
     /// <summary>
@@ -25,7 +26,7 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
+            // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
