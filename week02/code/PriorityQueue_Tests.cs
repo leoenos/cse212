5624bdaee414d314d55bcee4df3b5cb81069fffc
leoenos <senos@byupathway.edu>
2026-09-19 16:10:22 -0600

    test method 1 priotity dequeue

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,13 +6,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Three items are added to the queue with different priorities. Highest priority valued item gets dequued.
+    // Expected Result: The item with the highest priority is dequeued first.
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        
+        priorityQueue.Enqueue("Item1", 1);
+        priorityQueue.Enqueue("Item2", 5);
+        priorityQueue.Enqueue("Item3", 3);
+
+        var result = priorityQueue.Dequeue();
+
+        Assert.AreEqual("Item2", result);
     }
 
     [TestMethod]
